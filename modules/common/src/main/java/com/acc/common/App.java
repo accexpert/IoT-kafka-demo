9921a5121dd83893e1adf95e5f8786f2fb5dfff7
Cristi Ando Ciupav <accexpert@gmail.com>
2019-01-05 12:10:52 +0200

    first commit

---
+++ modules/common/src/main/java/com/acc/common/App.java
@@ -0,0 +1,13 @@
+package com.acc.kafkademo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
